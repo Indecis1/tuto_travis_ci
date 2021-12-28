eb7135042ed183633b13b501e85405981ab744f6
Indecis1 <sopalwyn@gmail.com>
2021-12-28 07:24:15 +0100

    Add all functionalities of the calculator

--- src/main/java/com/example/controller/CalculatorController.java
+++ src/main/java/com/example/controller/CalculatorController.java
@@ -1,11 +1,9 @@
 package com.example.controller;
 
 
+import com.example.utils.ModuloOperation;
 import com.example.utils.Result;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -13,13 +11,41 @@ public class CalculatorController {
 
     @GetMapping(path="")
     public String home(){
-        return "Test";
+        return "Veuillez allez sur le chemin correspondant à votre opération\n1-addition\n2-soustraction\n3-multiplication\n4-division\n5-modulo";
     }
 
     @GetMapping(path="addition/{x}/{y}")
     public Result addition(@PathVariable double x, @PathVariable double y){
         Result result = new Result();
         result.setResult(x + y);
+        return result;
+    }
+
+    @GetMapping(path="soustraction/{x}/{y}")
+    public Result soustraction(@PathVariable double x, @PathVariable double y){
+        Result result = new Result();
+        result.setResult(x - y);
+        return result;
+    }
+
+    @GetMapping(path="multiplication/{x}/{y}")
+    public Result multiplication(@PathVariable double x, @PathVariable double y){
+        Result result = new Result();
+        result.setResult(x * y);
+        return result;
+    }
+
+    @GetMapping(path="division/{x}/{y}")
+    public Result division(@PathVariable double x, @PathVariable double y){
+        Result result = new Result();
+        result.setResult(x / y);
+        return result;
+    }
+
+    @PostMapping(path="modulo")
+    public Result modulo(@RequestBody ModuloOperation operation){
+        Result result = new Result();
+        result.setResult(operation.getX() % operation.getY());
         return result;
     }
 
